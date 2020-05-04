832d21374bac786422156b1ce5ee68b210f791db
mohan355 <mkafle2035@gmail.com>
2020-05-04 18:05:34 -0400

    comment

---
+++ mohan_class/src/mohan_class/practise_home.java
@@ -0,0 +1,378 @@
+package mohan_class;
+
+public class practise_home {
+
+	public static void main(String[] args) {
+		
+	//	System.out.println("Hello World");
+		
+	/*	int a=5;
+		int b=15;
+		int c=a+b;
+		int d=a*b;
+		int e=b-a;
+		int f=b/a;
+		int g=b%a;
+		System.out.println("The value of a:"+a);
+		System.out.println("The value of b:"+b);
+		//Addition
+		System.out.println("The value of c:"+c);
+		//multiplication
+		System.out.println("The value of d:"+d);
+		//subtraction
+		System.out.println("The value of e:"+e);
+		// Division
+		System.out.println("The value of f:"+f);
+		
+		System.out.println("The value of g:"+g);
+		System.out.println();
+		*/
+		
+		// using AND operator
+		
+	/*	int a=32;
+		int b=8;
+		int c=8;
+		
+		boolean d=(a==b)&&(b==c);
+		System.out.println("The value of d:"+d);
+		
+		// Using OR operator
+		
+		boolean e=(a<b)||(b<a);
+		System.out.println("the value of e:"+e);
+		
+		// Using Not operator
+		
+		boolean f=!(a==b);
+		System.out.println("The value of f:"+f);
+		*/
+		
+	/*	int x=20;
+		int y=30;
+		if(x<y) {
+		System.out.println("false");
+			
+		}
+		else {
+			System.out.println("true");
+		}
+		*/
+	/*	int a=100;
+		
+		if(a==100||a>100) {
+			System.out.println("the value of a:"+a);
+		}
+		else {
+			System.out.println("the condition is not true:");
+		}
+			
+	/*	if(a==100 && a>100) {
+			System.out.println("the value of a:"+a);
+		}
+		else {
+			System.out.println("the value of a:"+a);
+		}  
+		*/
+	//int a=00;
+		//
+		//
+		//int b=000;
+	//	int c=0000;
+		
+		
+		/*	if(a>c) {
+				System.out.println("the bigger is a");
+			}
+			else {
+				System.out.println("bigger is c");
+			}
+			 if (b>c) {
+				System.out.println("the bigger is b");
+			}
+			else {
+				System.out.println("the bigger is c");
+			}
+			*/
+	/*	if(a>b) 
+			System.out.println("the bigger is a");
+		}
+		else if(b>c);{
+			System.out.println("the bigger is b");
+			
+		}
+		else {
+			System.out.println("c is bigger");
+		}
+			*/
+		/*if(a>b) {
+			if(a>c) {
+				System.out.println("the bigger is a");
+			}
+			else if {
+				System.out.println("the bigger ic c");
+			}
+			else if(b>c) {
+				System.out.println("b is bigger");
+			}
+			else {
+				System.out.println("c is bigger");
+			}
+			
+			*/
+	 /*  int a=10;
+		int b=8;
+		int c=3;
+		
+		
+		if(a>b) {
+			if(a>c) {
+				System.out.println("a is bigger");
+			}
+			else {
+				System.out.println("c is bigger");
+			}
+			}else if(b>c) {
+				System.out.println("bigger is b");
+			}else {
+				System.out.println("bigger is c");
+			}
+		
+			
+			
+		
+			
+			
+		/*	String firstName = "mohan ";
+			String lastName =  "kafle";
+			String fullName = firstName + lastzName;
+			System.out.println(fullName);
+					*/
+		/*	int x=5,y=6;
+			
+			int z= x+y;
+			//addition
+			//System.out.println("the sum of x and y :" +z);
+			
+			*/
+			
+	/*	int a=4;
+		int b=8;
+		int c=3;
+			
+			*/
+			
+			
+		/*	int a=00;
+			int b=000;
+			int c=0000;
+			
+			if(a>b) {
+				
+				
+				
+				
+				
+				
+				
+			}
+			*/
+		
+	/*	int a=1;
+		int c=a--;
+		System.out.println(a);
+		System.out.println(c);
+		
+		*/
+		
+	/*	int a=0;
+		if(a>0)
+		{
+			System.out.println("a is positive");
+		}
+		else if(a>0){
+			System.out.println("a is negative");
+		}
+		else {
+			System.out.println("a is zero");
+		}
+		*/
+		
+	/*	int a=10;
+		int b=8;
+		int c=15;
+		
+		if(a>b) {
+			if(a>c) {
+				System.out.println("a is biggest");
+			}
+		}
+			else if(c>a) {
+				if(c>b) {
+					System.out.println("c is bigest");
+				}
+				
+			}
+			else {
+				System.out.println("b is biggest");
+			}
+				
+		}
+		*/
+		
+		
+		
+		
+		
+		
+		
+		
+	/*	int a=20;
+		int b=50;
+		int c=50;
+		int d = 100;
+		
+		
+		if(a>b)
+		{
+			if(a>c)
+			{
+				if(a>d)
+				{
+				System.out.println("a is biggest");
+				}
+			}
+			
+		}
+		else if (b>c)
+		{
+			if (b > a)
+			{
+				if(b>d)
+				{
+				System.out.println("b is biggest");
+				}
+			}
+		}
+		else if (c>d)
+		{
+			if (c > a)
+			{
+				if( c > b)
+				{
+				System.out.println("c is biggest");
+				}
+			}
+		}
+		else
+		{
+			System.out.println("d is biggest");
+		}
+		*/
+		
+		
+		
+	/*
+		int a=18;
+		int b=15;
+		int c=12;
+		
+		if(a>b) {
+		
+			if(a>c) {
+				System.out.println("a is biggest");
+				
+			}
+		}
+		else if(b>c) {
+			if(b>a) {
+				System.out.println("b is biggest");
+			}
+			
+			
+		}
+		else {
+			System.out.println("c is biggest");
+		}
+		
+*/
+	//	System.out.println(" the number are: ");
+		
+	
+		
+	/*	int i = 11;
+	while ( i < 10)
+	{
+	
+		System.out.println(i);
+		i++;
+	}
+	
+		
+	/*	do
+		{
+			System.out.println(i);
+		}while(i<10);
+		
+		
+		
+	/*	int day= 3;
+		
+		switch(day)
+		{
+			case 1:
+			{
+				System.out.println("the day is sunday");
+				
+			}
+			break;
+			case 2:
+			{
+				System.out.println("the day is monday");
+				break;
+			}
+			
+			case 3:{
+				System.out.println("the day is tuesday");
+				break;
+			}
+			default:
+			{
+				System.out.println("no value given");
+				break;
+			}
+		}
+		
+		*/
+		
+	/*	 i=40;
+		while ( i < 40)
+		{
+		
+			System.out.println(i);
+			i--;
+		}
+		
+		
+		/*
+			
+			do
+			{
+				System.out.println(a);
+				a+=3;
+			}while(a>30);
+			
+		*/
+	/*	int a=40;
+		while(a<40) {
+			System.out.println(a);
+			a++;
+		}
+		*/
+		
+		
+		}
+		
+	
+	}
+
